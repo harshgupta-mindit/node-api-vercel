ca7e9ac1c435d4392a4aa75bf58c8ca2efbeeefb
harshgupta-mindit <harsh.gupta@minditsystems.com>
2023-08-03 14:25:43 +0530

    CORS shifted to app.js from server.js

--- app.js
+++ app.js
@@ -4,8 +4,28 @@ const app= express();
 const morgan = require("morgan");
 app.use(morgan("dev"));
 
+
+// CORS Handling
+app.use((req, res, next)=>{
+   res.header('Access-Control-Allow-Origin', "*");
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Authorization');
+
+   if(req.method === 'OPTIONS'){
+       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+
+       return res.status(200).json({});
+   }
+
+   // Dont forget to apply "next" method in this, because without this, after this methods route wouldn't run
+   next();
+})
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+
 
 const productRoute = require("./routes/productRoutes.js");
 const orderRoute = require("./routes/orderRoutes.js");
